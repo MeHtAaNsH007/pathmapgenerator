dc42725a8081f07452084f659662ee96d3c8e8e3
ansh mehta <anshmehta@Bruce-Wayne.local>
2026-02-16 00:19:34 +0530

    Integrated Tailwind UI for PROGATH

--- Desktop/first-hackathon/src/app/dashboard/page.tsx
+++ Desktop/first-hackathon/src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = 'https://jtbyrnlbbxnjyipxajmt.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0YnlybmxiYnhuanlpcHhham10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5OTExMzMsImV4cCI6MjA4NjU2NzEzM30.xBKQ0Q2t54E93Ra_SEvi9cPp-1WtwvYmQU-ByWTydwQ';
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const SAVED_ROADMAPS_KEY = "progath_saved_roadmaps";
